b7f64e1c5d7c2735148040ed861068a0319d092b
AJAmran <mdamranhossen77@gmail.com>
2026-05-11 00:21:39 +0600

    Building GET Method

--- mission 02/module - 06/src/controller/product.controller.ts
+++ mission 02/module - 06/src/controller/product.controller.ts
@@ -1,5 +1,6 @@
 import type { IncomingMessage, ServerResponse } from "node:http";
 import { readProduct } from "../service/product.service";
+import type { IProduct } from "../types/product.type";
 
 export const productController = (
   req: IncomingMessage,
@@ -7,9 +8,22 @@ export const productController = (
 ) => {
   const url = req.url;
   const method = req.method;
+  const urlParts = url?.split("/");
+  const id =
+    urlParts && urlParts[1] === "products" ? Number(urlParts[2]) : null;
+  console.log(id);
   if (url === "/products" && method === "GET") {
     const product = readProduct();
     res.writeHead(200, { "content-type": "application/json" });
+    res.end(
+      JSON.stringify({
+        message: "Retrive Product Successfully",
+        data: { product },
+      }),
+    );
+  } else if (method === "GET" && id !== null) {
+    const products = readProduct();
+    const product = products.find((product: IProduct) => product.id === id);
     res.end(
       JSON.stringify({
         message: "Retrive Product Successfully",
