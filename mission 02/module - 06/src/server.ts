d82c1b3e432e5ef520aab4f6e6f885af1bb1364f
AJAmran <mdamranhossen77@gmail.com>
2026-05-10 23:13:15 +0600

    better file ogr

--- mission 02/module - 06/src/server.ts
+++ mission 02/module - 06/src/server.ts
@@ -4,21 +4,11 @@ import {
   Server,
   ServerResponse,
 } from "node:http";
+import { routeHandler } from "./routes/route";
 
 const server: Server = createServer(
   (req: IncomingMessage, res: ServerResponse) => {
-    const url = req.url;
-    const method = req.method;
-    if (url === "/" && method == "GET") {
-      res.writeHead(200, { "content-type": "application/json" });
-      res.end(JSON.stringify({ message: "This is root route" }));
-    } else if (url?.startsWith("/products")) {
-      res.writeHead(200, { "content-type": "application/json" });
-      res.end(JSON.stringify({ message: "This is Product route" }));
-    } else {
-      res.writeHead(404, { "content-type": "text/plain" });
-      res.end(JSON.stringify({ message: "Route Not Found" }));
-    }
+    routeHandler(req, res);
   },
 );
 
