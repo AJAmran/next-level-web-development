201e1f01b19434841c73cf7f47d6b94862e05e6f
AJAmran <mdamranhossen77@gmail.com>
2026-05-13 21:43:45 +0600

    Create server with express & Typescript

---
+++ mission 02/module - 7/src/server.ts
@@ -0,0 +1,11 @@
+import express, { type Request, type Response } from "express";
+const app = express();
+const port = 5000;
+
+app.get("/user", (req: Request, res: Response) => {
+  res.send("Hello World......!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
