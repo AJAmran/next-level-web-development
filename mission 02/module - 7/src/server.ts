5759674c7d4dd9c7bca47b44a0f90aef2845bb16
AJAmran <mdamranhossen77@gmail.com>
2026-05-15 18:12:44 +0600

    Executing Pool and creating tables

--- mission 02/module - 7/src/server.ts
+++ mission 02/module - 7/src/server.ts
@@ -6,7 +6,7 @@ import express, {
 import { Pool } from "pg";
 
 const app: Application = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(express.text());
@@ -16,6 +16,29 @@ const pool = new Pool({
   connectionString:
     "postgresql://neondb_owner:npg_ewhBXpjrv17s@ep-frosty-boat-apd7iled-pooler.c-7.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
 });
+
+const initDB = async () => {
+  try {
+    await pool.query(`
+      CREATE TABLE IF NOT EXISTS users (
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(20),
+        email VARCHAR(20),
+        password VARCHAR(20) NOT NULL,
+        is_active BOOLEAN DEFAULT true,
+        age INT,
+        created_at TIMESTAMP DEFAULT NOW(),
+        updated_at TIMESTAMP DEFAULT NOW()
+      )
+    `);
+
+    console.log("Table created successfully");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+initDB();
 
 app.get("/user", (req: Request, res: Response) => {
   //   res.send("Hello World......!");
