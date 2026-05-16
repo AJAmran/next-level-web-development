86fd94e0670e075881093e99817254720fd9d8a3
AJAmran <mdamranhossen77@gmail.com>
2026-05-17 00:06:25 +0600

    fixed id issue

--- mission 02/module - 7/src/modules/users/user.controller.ts
+++ mission 02/module - 7/src/modules/users/user.controller.ts
@@ -1,5 +1,4 @@
 import type { Request, Response } from "express";
-import { pool } from "../../db";
 import { userService } from "./user.service";
 
 const getUsers = async (req: Request, res: Response) => {
--- mission 02/module - 7/src/modules/users/user.route.ts
+++ mission 02/module - 7/src/modules/users/user.route.ts
@@ -1,14 +1,14 @@
-import { Router, type Request, type Response } from "express";
+import { Router } from "express";
 import { userController } from "./user.controller";
 
 
 const router = Router();
 
 router.get("/", userController.getUsers);
-router.get("/id", userController.getUser);
+router.get("/:id", userController.getUser);
 router.post("/", userController.createUser)
-router.put("/id", userController.updateUser);
-router.delete("/id", userController.deleteUser);
+router.put("/:id", userController.updateUser);
+router.delete("/:id", userController.deleteUser);
 
 
 export const userRoute = router
